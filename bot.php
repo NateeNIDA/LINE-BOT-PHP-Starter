86caf36809fbdf4432d832281e0bf64a9efd7219
NateeNIDA <natee.sus@stu.nida.ac.th>
2017-04-26 16:44:31 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -55,6 +55,57 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 		}
+		
+		if ($event['type'] == 'location') {
+			
+			$latitude = $event['message']['latitude'];
+			$latitude = $event['message']['latitude'];
+			
+			// Get text sent
+			$text = "บันทึก Location เรียบร้อยแล้ว";
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			
+			// Step 1
+			$cSession = curl_init();
+			// Step 2
+			curl_setopt($cSession,CURLOPT_URL,"http://1.179.187.126/linegps/insertpoi.php?gpsname=test&gpsgroup=olt&gpslat=$latitude&gpslong=$longitutde");
+			curl_setopt($cSession,CURLOPT_RETURNTRANSFER,true);
+			curl_setopt($cSession,CURLOPT_HEADER, false);
+			// Step 3
+			$result=curl_exec($cSession);
+			// Step 4
+			curl_close($cSession);
+			// Step 5
+			echo $result;
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+		
 	}
 }
 echo "OK";
