0464f8c401a75f922916784fb88afa7b7ff54069
NateeNIDA <natee.sus@stu.nida.ac.th>
2017-04-26 20:26:46 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message') {
 			// Get text sent
-			$text = $event['message']['text'];
+			$text = "userID : ".$event['message']['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
