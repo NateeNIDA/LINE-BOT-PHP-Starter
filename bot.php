46d725d46b6bd9884d334273068e920e3ef41611
NateeNIDA <natee.sus@stu.nida.ac.th>
2017-04-27 09:31:44 +0700

    bug6

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 
-function defalutReply(&$event){
+function defalutReply(&$event,$access_token){
 	// Get text sent
 	$text = "userID : ".$event['source']['userId'];
 	// Get replyToken
@@ -35,7 +35,7 @@ function defalutReply(&$event){
 	echo $result . "\r\n";
 }
 
-function saveGps1(&$event){
+function saveGps1(&$event,$access_token){
 
 	// Get text sent
 	$text = "userID : ".$event['source']['userId'];
@@ -85,39 +85,9 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message') {
 
 			if ($event['message']['type'] == 'location') {
-				saveGps1($event);
+				saveGps1($event,$access_token);
 			}else{
-				// Get text sent
-				$text = "userID : ".$event['source']['userId'];
-				// Get replyToken
-				$replyToken = $event['replyToken'];
-
-				// Build message to reply back
-				$messages = [
-					'type' => 'text',
-					'text' => $text
-				];
-
-				// Make a POST Request to Messaging API to reply to sender
-				$url = 'https://api.line.me/v2/bot/message/reply';
-				$data = [
-					'replyToken' => $replyToken,
-					'messages' => [$messages],
-				];
-				$post = json_encode($data);
-				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-
-
-				$ch = curl_init($url);
-				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-				$result = curl_exec($ch);
-				curl_close($ch);
-
+				defalutReply($event,$access_token);
 			}
 
 		}
