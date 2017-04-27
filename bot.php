2f231714dc8fe6c18a6cfbebec4887a5045a23a8
NateeNIDA <natee.sus@stu.nida.ac.th>
2017-04-27 09:23:39 +0700

    bug4

--- bot.php
+++ bot.php
@@ -1,75 +1,74 @@
 <?php
 
 function defalutReply(&$event){
-	if ($event['type'] == 'message') {
-		// Get text sent
-		$text = "userID : ".$event['source']['userId'];
-		// Get replyToken
-		$replyToken = $event['replyToken'];
+	// Get text sent
+	$text = "userID : ".$event['source']['userId'];
+	// Get replyToken
+	$replyToken = $event['replyToken'];
 
-		// Build message to reply back
-		$messages = [
-			'type' => 'text',
-			'text' => $text
-		];
+	// Build message to reply back
+	$messages = [
+		'type' => 'text',
+		'text' => $text
+	];
 
-		// Make a POST Request to Messaging API to reply to sender
-		$url = 'https://api.line.me/v2/bot/message/reply';
-		$data = [
-			'replyToken' => $replyToken,
-			'messages' => [$messages],
-		];
-		$post = json_encode($data);
-		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
+	// Make a POST Request to Messaging API to reply to sender
+	$url = 'https://api.line.me/v2/bot/message/reply';
+	$data = [
+		'replyToken' => $replyToken,
+		'messages' => [$messages],
+	];
+	$post = json_encode($data);
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		$result = curl_exec($ch);
-		curl_close($ch);
 
-		echo $result . "\r\n";
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$result = curl_exec($ch);
+	curl_close($ch);
+
+	echo $result . "\r\n";
 }
 
 function saveGps1(&$event){
 
-		// Get text sent
-		$text = "userID : ".$event['source']['userId'];
-		// Get replyToken
-		$replyToken = $event['replyToken'];
+	// Get text sent
+	$text = "userID : ".$event['source']['userId'];
+	// Get replyToken
+	$replyToken = $event['replyToken'];
 
-		// Build message to reply back
-		$messages = [
-			'type' => 'text',
-			'text' => $text
-		];
+	// Build message to reply back
+	$messages = [
+		'type' => 'text',
+		'text' => $text
+	];
 
-		// Make a POST Request to Messaging API to reply to sender
-		$url = 'https://api.line.me/v2/bot/message/reply';
-		$data = [
-			'replyToken' => $replyToken,
-			'messages' => [$messages],
-		];
-		$post = json_encode($data);
-		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+	// Make a POST Request to Messaging API to reply to sender
+	$url = 'https://api.line.me/v2/bot/message/reply';
+	$data = [
+		'replyToken' => $replyToken,
+		'messages' => [$messages],
+	];
+	$post = json_encode($data);
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 
 
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		$result = curl_exec($ch);
-		curl_close($ch);
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$result = curl_exec($ch);
+	curl_close($ch);
 
-		echo $result . "\r\n";
+	echo $result . "\r\n";
 }
 
 $access_token = 'TuW0H/TfMxwVBHwEi6Zt729tKALS9WMIj0KTS2ahufD2Yttfk9E2yZQqGjrucaIguFuB2DdxnnXQ2QrN7Im9n/Yv5lwr4q7a1952aLTyJJEVm1F97R9GjbxoWm/iOhUwxanRbPWXsec15hNJxNAJXgdB04t89/1O/w1cDnyilFU=';
@@ -83,18 +82,18 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-			if ($event['type'] == 'message') {
+		if ($event['type'] == 'message') {
 			defalutReply($event);
 		}
 
-			if ($event['message']['type'] == 'location') {
-				saveGps1($event);
-
-			}
+		if ($event['message']['type'] == 'location') {
+			saveGps1($event);
 
 		}
 
-
 	}
+
+
 }
+
 echo "OK";
